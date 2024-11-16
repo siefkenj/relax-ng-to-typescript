fd2e9a84093d1d6c4d439b12e69cf9e59761268d
Jason Siefken <siefkenj@gmail.com>
2024-11-16 16:33:55 -0500

    Add build scripts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./xast-utils";
+export { doSimplificationPlugin } from "./relax-ng/simplification/do-simplification-plugin";
+export { makeTypesForGrammar } from "./relax-ng/typescript/make-type";
+export { renameRefsPlugin } from "./relax-ng/typescript/rename-refs-plugin";
